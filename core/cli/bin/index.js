fa3d53f3679da73ff8dec7c3532a3236d7854dc3
dengzhanyong <2657179843@qq.com>
2025-06-25 17:55:01 +0800

    feat: 修改文案

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -3,7 +3,7 @@
 const importLocal = require('import-local');
 
 if (importLocal(__filename)) {
-    console.log('执行本地脚手架');
+    console.log('执行本地脚手架!!');
 } else {
     require('../lib/index')(process.argv.slice(2))
 }
